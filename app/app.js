87599e4049b211fb55c399ad1115e1b2bafc1b81
Ji Tao <jitao75@gmail.com>
2018-11-21 14:43:35 +0800

    using webpack after tsc

--- app/app.js
+++
@@ -1,6 +0,0 @@
-console.log("I'm working");
-function startGame() {
-    var messageElement = document.getElementById('message');
-    messageElement.innerText = "Welcome to MultiMath Game!";
-}
-document.getElementById('startGame').addEventListener('click', startGame);
--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,17 @@
+import { Person } from './person';
+
+let traderMax: Person = new Person('max',2000);
+
+let x: string = "Whose banana?";
+
 console.log("I'm working")
-function startGame(){
-    var messageElement = document.getElementById('message');
-    messageElement.innerText = "Welcome to MultiMath Game!"
+
+//start a game
+function startGame():void{
+    let messageElement: any; 
+    messageElement = document.getElementById('message');
+    messageElement.innerText = `Welcome to ${x} MultiMath Game! ${traderMax.name}`
 }
 
 document.getElementById('startGame').addEventListener('click',startGame);
+console.log("talking");
